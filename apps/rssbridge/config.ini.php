53549eb56fcfbd0758ddaf692815314303d2e338
Guilhem Niot <guilhem.niot@gmail.com>
2020-09-11 15:54:26 +0200

    Expose my server setup

---
+++ apps/rssbridge/config.ini.php
@@ -0,0 +1,16 @@
+[authentication]
+
+; Enables authentication for all requests to this RSS-Bridge instance.
+;
+; Warning: You'll have to upgrade existing feeds after enabling this option!
+;
+; true  = enabled
+; false = disabled (default)
+enable = true
+
+; The username for authentication. Insert this name when prompted for login.
+username = "myusername"
+
+; The password for authentication. Insert this password when prompted for login.
+; Use a strong password to prevent others from guessing your login!
+password = "mylongandsecurepassword"
